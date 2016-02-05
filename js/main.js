7830cb48f8e86fa0540385cb20efe590e1100e01
Ashley Childers <ashley.childers@gmail.com>
2016-02-04 21:42:29 -0500

    Add presidents branch and presidents array to js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ var iceCream = [
     'chocolate chip cookie dough', 'rocky road', 'vanilla',
     'mint chocolate chip'
 ];
+
+var Presidents = [
+    { firstName: "Barack", lastName: "Obama", termLength: 2, party: "democrat", yearsofPresidency: 7 },
+    { firstName: "George W", lastName: "Bush", termLength: 2, party: "republican", yearsofPresidency: 8 },
+    { firstName: "Bill", lastName: "Clinton", termLength: 2, party: "democrat", yearsofPresidency: 8 },
+    { firstName: "George H", lastName: "Bush", termLength: 1, party: "republican", yearsofPresidency: 4 },
+    { firstName: "Ronald", lastName: "Reagan", termLength: 2, party: "republican", yearsofPresidency: 8 }
+];
