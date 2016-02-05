50950f57cfaf4378d861f39ba63fa9190f95658f
Ashley Childers <ashley.childers@gmail.com>
2016-02-04 23:13:58 -0500

    Add title to index and info to presidents array

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@ var iceCream = [
     'mint chocolate chip'
 ];
 
-var Presidents = [
-    { firstName: "Barack", lastName: "Obama", termLength: 2, party: "democrat", yearsofPresidency: 7 },
-    { firstName: "George W", lastName: "Bush", termLength: 2, party: "republican", yearsofPresidency: 8 },
-    { firstName: "Bill", lastName: "Clinton", termLength: 2, party: "democrat", yearsofPresidency: 8 },
-    { firstName: "George H", lastName: "Bush", termLength: 1, party: "republican", yearsofPresidency: 4 },
-    { firstName: "Ronald", lastName: "Reagan", termLength: 2, party: "republican", yearsofPresidency: 8 }
-];
+var presidents = [
+    { firstName: 'Barack', lastName: 'Obama', termLength: 2,
+        party: 'democrat', yearsofPresidency: 7 },
+    { firstName: 'George W', lastName: 'Bush', termLength: 2,
+        party: 'republican', yearsofPresidency: 8 },
+    { firstName: 'Bill', lastName: 'Clinton', termLength: 2,
+        party: 'democrat', yearsofPresidency: 8 },
+    { firstName: 'George H', lastName: 'Bush', termLength: 1,
+        party: 'republican', yearsofPresidency: 4 },
+    { firstName: 'Ronald', lastName: 'Reagan', termLength: 2,
+     party: 'republican', yearsofPresidency: 8 }
+
+console.log( presidents[2]firstName + lastName);
+
+] ;
