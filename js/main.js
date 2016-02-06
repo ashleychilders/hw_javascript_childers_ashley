2eb51fc2bd7894a0658985302a9b4309f9ef8495
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-06 11:57:28 -0800

    add comments on additional ways to add to an array

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 var iceCream = [ 'chocolate chip cookie dough', 'rocky road', 'vanilla' ];
 iceCream.push( 'mint chocolate chip' ); // adds a 4th item at the end
+
+// additional ways to add items to an array:
+// iceCream[ iceCream.length ] = 'strawberry'; // at the end
+// iceCream.unshift( 'mint chocolate chip' ); // at the beginning
